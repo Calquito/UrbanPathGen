d47470ed113dc510beeaeb0cd0a270de1d0d25ac
Calquito <39011234+Calquito@users.noreply.github.com>
2023-11-26 14:42:59 -0600

    Add unit tests

---
+++ Trajectory Generation/testing/test_matrix_analysis.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('Trajectory Generation')
+
+import numpy as np
+import pytest
+from matrix_analysis import get_middle_horizontal_coordinates
+
+@pytest.mark.parametrize("binary_matrix, min_size, expected_coordinates", [
+    (np.array([[1, 1, 1, 1, 1, 1, 0, 1, 0],
+               [1, 1, 0, 0, 0, 0, 0, 1, 0],
+               [0, 1, 1, 1, 1, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 1, 0],
+               [1, 0, 1, 0, 1, 1, 1, 0, 0],
+               [0, 0, 1, 0, 0, 0, 1, 1, 0],
+               [1, 0, 1, 0, 0, 0, 0, 0, 0],
+               [1, 0, 1, 0, 0, 0, 0, 1, 0]]), 3, [(1, 2), (5, 2), (4, 5)]),
+
+])
+def test_get_middle_horizontal_coordinates(binary_matrix, min_size, expected_coordinates):
+    result = get_middle_horizontal_coordinates(binary_matrix, min_size)
+    
+    assert result == expected_coordinates
